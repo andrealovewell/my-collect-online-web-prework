6fa073f5339f4e9dec635b2d365be87006e4c2de
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-17 06:35:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,6 @@
 
-def my_collect(array)
-  i = 0
-  new_array = []
-  while (i < array.length)
-  new_array << yield(array[i].upcase)
-    i += 1
+def my_collect(student)
+  student.collect do |i|
+    student.upcase
   end
-  new_array
 end
